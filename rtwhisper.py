a1a586595c48fdc72adc379fc5b35c9079787c1b
Deepesh Kumar Jha <75719377+Deepesh1024@users.noreply.github.com>
2024-12-31 22:56:19 +0530

    Update rtwhisper.py

--- rtwhisper.py
+++ rtwhisper.py
@@ -13,7 +13,7 @@ class Config:
                 f"Environment variable '{env_variable_name}' not found."
             )
 
-#model function
+#model class
 class GroqClientManager:
     def __init__(self, api_key: str):
         self.api_key = api_key

d165544554a317846fdd47ec6f7334e4d4947baa
Deepesh Kumar Jha <75719377+Deepesh1024@users.noreply.github.com>
2024-12-31 22:53:45 +0530

    Update rtwhisper.py

--- rtwhisper.py
+++ rtwhisper.py
@@ -3,6 +3,7 @@ import speech_recognition as sr
 from groq import Groq
 from dotenv import load_dotenv
 
+
 class Config:
     def __init__(self, env_variable_name="GROQ_API_KEY"):
         load_dotenv()  # Load environment variables from .env if present
